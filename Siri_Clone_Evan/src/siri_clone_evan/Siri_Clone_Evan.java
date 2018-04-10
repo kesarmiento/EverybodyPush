c099475cead920449829f01167520d16606f26ab
rwens_000 <rwens_000@104.39.222.106>
2018-04-10 14:26:03 -0400

    I added "This is Evan's version"

---
+++ Siri_Clone_Evan/src/siri_clone_evan/Siri_Clone_Evan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package siri_clone_evan;
+
+/**
+ *
+ * @author rwens_000
+ */
+public class Siri_Clone_Evan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is Evan Hello");
+    }
+    
+}
